c3cba067abf6731d5ad1a4dad53074c40a352039
antden99 <antoniodenigris@icloud.com>
2024-05-08 15:03:08 +0200

    add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,25 +6,27 @@
         <section class="jumbo">
         </section>
 
-        <section class="current_series bg-dark">
+        <section class="current_series">
             <div class="container">
                 <div class="row row-cols-1 row-cols-md-6 g-4">
-                    @foreach ($arrayList as $array )
-                    <div class="col">
-                        <div class="card border-0">
-                            <img class="card-img-top" src="{{$array['thumb']}}" alt="image describing the following array {{$array['title']}}">
-                            <div class="card-body">
-                                {{$array['title']}}
+                    @foreach ($arrayList as $array)
+                        <div class="col">
+                            <div class="card border-0">
+                                <img class="card-img-top" src="{{ $array['thumb'] }}"
+                                    alt="image describing the following array {{ $array['title'] }}">
+                                <div class="card-body">
+                                    {{ $array['title'] }}
+                                </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
+                <div class="text-center p-4"><button type="button" class="btn btn-primary text-white">LOAD MORE</button></div>
         </section>
 
         <section class="digital_comics text-white ">
             <div class="container">
-                <ul class="list-unstyled d-flex justify-content-between p-5 ">
+                <ul class="list-unstyled d-flex justify-content-between p-5 m-0">
                     <li>
                         <img src="{{ Vite::asset('resources/img/cell.png') }}" alt=""> DIGITAL COMICS
                     </li>
